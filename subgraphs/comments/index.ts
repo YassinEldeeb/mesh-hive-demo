4081882f2e68006eea0a711bc9c240f30a2d85b8
Dotan Simha <dotansimha@gmail.com>
2024-07-18 14:20:38 +0300

    wip

--- subgraphs/comments/index.ts
+++ subgraphs/comments/index.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from "fs";
 import { createServer } from "http";
 import { join } from "path";
-import { parse, print } from "graphql";
-import { createYoga, Plugin, YogaLogger } from "graphql-yoga";
+import { parse } from "graphql";
+import { createYoga } from "graphql-yoga";
 import { buildSubgraphSchema } from "@apollo/subgraph";
-import { HMACUpstreamSignatureValidationOptions } from "@graphql-mesh/hmac-upstream-signature";
+import { useHMACSignatureValidation } from "@graphql-mesh/hmac-upstream-signature";
 
 const comments = [
   { id: "1", text: "Great post!", author: { id: "1" } },
@@ -17,34 +17,6 @@ if (!HMAC_SIGNING_SECRET) {
   throw new Error("HMAC_SIGNING_SECRET environment variable is required");
 }
 
-function createCryptoKey(
-  secret: string,
-  usages: KeyUsage[]
-): Promise<CryptoKey> {
-  return crypto.subtle.importKey(
-    "raw",
-    new TextEncoder().encode(secret),
-    { name: "HMAC", hash: "SHA-256" },
-    false,
-    usages
-  );
-}
-const DEFAULT_HEADER_NAME = "x-hmac-signature";
-
-export function useHMACSignatureValidation(
-  options: HMACUpstreamSignatureValidationOptions
-): Plugin {
-  const headerName = options.headerName || DEFAULT_HEADER_NAME;
-  const key$ = createCryptoKey(options.secret, ["verify"]);
-  let logger: YogaLogger;
-
-  return {
-    onYogaInit({ yoga }) {
-      logger = yoga.logger;
-    },
-  };
-}
-
 createServer(
   createYoga({
     logging: true,
@@ -52,11 +24,6 @@ createServer(
       useHMACSignatureValidation({
         secret: HMAC_SIGNING_SECRET,
       }),
-      {
-        onExecute: ({ args }: any) => {
-          console.log("onExecute", print(args.document));
-        },
-      },
     ],
     schema: buildSubgraphSchema({
       typeDefs: parse(
--- subgraphs/users/index.ts
+++ subgraphs/users/index.ts
@@ -2,20 +2,55 @@ import { readFileSync } from "fs";
 import { createServer } from "http";
 import { join } from "path";
 import { parse, print } from "graphql";
-import { createYoga } from "graphql-yoga";
+import { createYoga, Plugin } from "graphql-yoga";
 import { buildSubgraphSchema } from "@apollo/subgraph";
 import { useHMACSignatureValidation } from "@graphql-mesh/hmac-upstream-signature";
+import { useOperationFieldPermissions } from "@envelop/operation-field-permissions";
 
 const users = [
   { id: "1", name: "Alice" },
   { id: "2", name: "Bob" },
 ];
 
+const permissionsPerUserId: Record<string, Set<string>> = {
+  "1": new Set(["Query.*", "User.id", "User.__typename"]),
+  "2": new Set(["Query.*", "User.id", "User.__typename", "User.name"]),
+};
+
 const { HMAC_SIGNING_SECRET } = process.env;
 
 if (!HMAC_SIGNING_SECRET) {
   throw new Error("HMAC_SIGNING_SECRET environment variable is required");
 }
+
+const useMyPlugin = (): Plugin<{ currentUserId: string }> => {
+  const mapping = new WeakMap<Request, Record<string, any>>();
+
+  return {
+    onParams({ params, request }) {
+      console.log("onParams, request extensions:", params.extensions);
+
+      if (params.extensions) {
+        mapping.set(request, params.extensions);
+      }
+    },
+    onContextBuilding({ context, extendContext }) {
+      const header = context.request.headers.get("x-hmac-signature");
+      console.log("x-hmac-signature:", header);
+
+      if (mapping.has(context.request)) {
+        const jwtRaw = mapping.get(context.request)!.jwt.payload;
+
+        extendContext({
+          currentUserId: jwtRaw.sub,
+        });
+      }
+    },
+    onExecute: ({ args }: any) => {
+      console.log("onExecute", print(args.document));
+    },
+  };
+};
 
 createServer(
   createYoga({
@@ -24,11 +59,19 @@ createServer(
       useHMACSignatureValidation({
         secret: HMAC_SIGNING_SECRET,
       }),
-      {
-        onExecute: ({ args }: any) => {
-          console.log("onExecute", print(args.document));
+      useMyPlugin(),
+      useOperationFieldPermissions<{ currentUserId: string }>({
+        // we can access graphql context here
+        getPermissions: async (context) => {
+          console.log("getPermissions, currentUserId:", context.currentUserId);
+
+          const permissions =
+            permissionsPerUserId[context.currentUserId] || new Set();
+          console.log("user permissions:", permissions);
+
+          return permissions;
         },
-      },
+      }),
     ],
     schema: buildSubgraphSchema({
       typeDefs: parse(
