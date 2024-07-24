7089c0145737f828037b7af5677a57138515ee5e
Dotan Simha <dotansimha@gmail.com>
2024-07-24 12:56:50 +0300

    switch comments subgraph to use apollo server, and added jwt claims extraction to apollo server

--- subgraphs/comments/index.ts
+++ subgraphs/comments/index.ts
@@ -1,10 +1,9 @@
 import { readFileSync } from "fs";
-import { createServer } from "http";
 import { join } from "path";
 import { parse } from "graphql";
-import { createYoga } from "graphql-yoga";
 import { buildSubgraphSchema } from "@apollo/subgraph";
-import { useHMACSignatureValidation } from "@graphql-mesh/hmac-upstream-signature";
+import { ApolloServer, ApolloServerPlugin } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 const comments = [
   { id: "1", text: "Great post!", author: { id: "1" } },
@@ -17,30 +16,50 @@ if (!HMAC_SIGNING_SECRET) {
   throw new Error("HMAC_SIGNING_SECRET environment variable is required");
 }
 
-createServer(
-  createYoga({
-    logging: true,
-    plugins: [
-      useHMACSignatureValidation({
-        secret: HMAC_SIGNING_SECRET,
-      }),
-    ],
-    schema: buildSubgraphSchema({
-      typeDefs: parse(
-        readFileSync(join(__dirname, "typeDefs.graphql"), "utf-8")
-      ),
-      resolvers: {
-        Query: {
-          comments: () => comments,
-        },
-        User: {
-          comments(user) {
-            return comments.filter((comment) => comment.author.id === user.id);
-          },
+const extractJwtPlugin = {
+  async requestDidStart({ request, contextValue }) {
+    contextValue.jwt = request.extensions?.jwt;
+  },
+} satisfies ApolloServerPlugin<{ jwt?: { payload: Record<string, any> } }>;
+
+const verifyHmacSignaturePlugin = {
+  async requestDidStart({ request }) {
+    const { headers } = request.http!;
+    const signature = headers.get("x-hmac-signature");
+
+    if (!signature) {
+      throw new Error("Missing HMAC signature");
+    }
+
+    // TODO: Fix this
+  },
+} satisfies ApolloServerPlugin;
+
+const server = new ApolloServer({
+  plugins: [verifyHmacSignaturePlugin, extractJwtPlugin],
+  schema: buildSubgraphSchema({
+    typeDefs: parse(readFileSync(join(__dirname, "typeDefs.graphql"), "utf-8")),
+    resolvers: {
+      Query: {
+        comments: () => comments,
+      },
+      User: {
+        comments(user, args, context) {
+          console.log("User.comments resolver called, context:", context);
+          return comments.filter((comment) => comment.author.id === user.id);
         },
       },
-    }),
-  })
-).listen(4002, () => {
-  console.log("Comments subgraph is running on http://localhost:4002");
+    },
+  }),
 });
+
+startStandaloneServer(server, {
+  listen: { port: 4002 },
+})
+  .then(({ url }) => {
+    console.log(`Comments subgraph is running on ${url}`);
+  })
+  .catch((error) => {
+    console.error("Failed to start Comments subgraph", error);
+    process.exit(1);
+  });
